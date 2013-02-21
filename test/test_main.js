05a22288d05375cc6963cc1c40cc07228c02f0ff
Sean Dunn <sd9@sanger.ac.uk>
2013-02-21 11:08:30 +0000

    Added support for S2Mapper integration

--- test/test_main.js
+++ test/test_main.js
@@ -3,20 +3,20 @@ require.config({
     },
 
   paths: {
-      domReady: 'components/requirejs-domready/domReady',
       d3: 'components/d3',
+      domReady: 'components/requirejs-domready/domReady',
+      mapper: 'components/S2Mapper/app/scripts/mapper',
+      mapper_test: 'components/S2Mapper/test',
       config: 'test_config',
       views: 'scripts/views',
       presenters: 'scripts/presenters',
       spec: 'spec',
-      json: 'json'
+      json: 'components/S2Mapper/test/json'
     },
 
-//    urlArgs: "bust=" + (new Date()).getTime(),
 });
- 
-require(['domReady!', 'spec/selection_page_model_spec', 'spec/selection_page_presenter_spec'], function() {
-  // use app here
-    runJasmineTests();
 
+require(["domReady!", "spec/selectionPageSpec"], function(document) {
+  // Very slight delay seems to be needed to get things synced...
+  window.setTimeout(runJasmineTests, 50);
 });
