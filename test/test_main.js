bb237a8e4b6630fd36691e5dfa2a27c311791a46
David Allen <da6@sanger.ac.uk>
2013-02-28 13:47:25 +0000

    Added CI configuration to the component

--- test/test_main.js
+++ test/test_main.js
@@ -25,8 +25,7 @@ require(['domReady!',
 	 'spec/selection_page_model_spec',
 	 'spec/selection_page_presenter_spec',
 	 'spec/scan_barcode_model_spec',
-	 'spec/scan_barcode_presenter_spec',
-	 'spec/tube_removal_presenter_spec'],
+	 'spec/scan_barcode_presenter_spec'],
  function() {
   // use app here
 
