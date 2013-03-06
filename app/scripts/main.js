94a094cf6a47d214633ec021c2a0fd2562e3754b
benoitmangili <benoit.mangili@tessella.com>
2013-03-06 08:41:34 +0000

    new labwarePresenter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,17 +17,23 @@ require.config({
   }
 });
 
-require(['extraction_pipeline/app'], function (app) {
+require(['extraction_pipeline/app','extraction_pipeline/presenters/labware_presenter', 'extraction_pipeline/presenters/presenter_factory'], function (app, LabwarePresenter, pf) {
     var theApp = new app();
   var inputModelForApp = undefined;
-  inputModelForApp = {
-    userUUID:"2345678901234",
-    labwareUUID:"106d61c0-6224-0130-90b6-282066132de2",
-    batchUUID:undefined //"1234567890"
-  };
+//  inputModelForApp = {
+//    userUUID:"2345678901234",
+//    labwareUUID:"106d61c0-6224-0130-90b6-282066132de2",
+//    batchUUID:undefined //"1234567890"
+//  };
     theApp.setupPresenter(inputModelForApp);
+
+
+
+
 //    var kitPresenter = new KitPresenter(undefined, new pf());
 //    kitPresenter.setupPresenter(undefined, function() { return $("#content"); });
+//    var lp = new LabwarePresenter(undefined, new pf());
+//    lp.setupPresenter(undefined, function() { return $("#content"); });
 
 });
 
--- test/spec/selection_page_model_spec.js
+++ test/spec/selection_page_model_spec.js
@@ -1,7 +1,7 @@
-define(['models/selection_page_model', 'spec/selection_page_helper', 'extraction_pipeline/dummyresource'], function(SelectionPageModel, SelectionPageHelper, DummyResource) {
+define(['models/selection_page_model', 'spec/selection_page_helper', 'extraction_pipeline/dummyresource'], function (SelectionPageModel, SelectionPageHelper, DummyResource) {
 
   'use strict';
-  
+
   var firstBatchUuid = '11111111-222222-00000000-111111111111';
   var secondBatchUuid = '11111111-222222-00000000-111111111112';
   var nextObjectUuid = '';
@@ -10,16 +10,16 @@ define(['models/selection_page_model', 'spec/selection_page_helper', 'extraction
   var owner = null;
   var barcodePresenter = {}; // minimal mock is acceptable
 
-  DummyResource.prototype.mutateJson = function(json) {
+  DummyResource.prototype.mutateJson = function (json) {
     console.log("patching tube");
-    
-   
+
+
     json.tube.uuid = nextObjectUuid;
-    if (nextBatchUuid !== '')  {
+    if (nextBatchUuid !== '') {
       json.tube.batch = {
-	rawJson:{
-          uuid: nextBatchUuid
-	}
+        rawJson:{
+          uuid:nextBatchUuid
+        }
       };
     }
     haveMutated = true;
@@ -28,196 +28,203 @@ define(['models/selection_page_model', 'spec/selection_page_helper', 'extraction
 
   }
 
-  var createSpyOwner = function() {
+  var createSpyOwner = function () {
     owner = {
-      childDone: function(child, action, data) {}
+      childDone:function (child, action, data) {
+      }
     };
-    spyOn(owner, 'childDone');    
+    spyOn(owner, 'childDone');
   }
 
 
-  describe("SelectionPageModel", function() {
+  describe("SelectionPageModel", function () {
 
     var helper = new SelectionPageHelper();
 
-    describe("model with 0 tubes", function() {
+    describe("model with 0 tubes", function () {
       var model;
 
-      beforeEach(function() { 
-	createSpyOwner();
+      beforeEach(function () {
+        createSpyOwner();
         model = new SelectionPageModel(owner, 123456789);
-	haveMutated = false;
+        haveMutated = false;
       });
 
-      it("has capacity of 12 tubes", function() {
+      it("has capacity of 12 tubes", function () {
         expect(model.getCapacity()).toEqual(12);
       });
 
-      it("has no batch identifier", function() {
+      it("has no batch identifier", function () {
         expect(model.batch).toBeUndefined();
       });
 
-      it("has batch identifier set after a tube has been added", function() {
-	var expectedBatchUuid = firstBatchUuid;
-	nextBatchUuid = firstBatchUuid;
-        runs(function() { 
+      it("has batch identifier set after a tube has been added", function () {
+        var expectedBatchUuid = firstBatchUuid;
+        nextBatchUuid = firstBatchUuid;
+        runs(function () {
           console.log("calling add tube");
-          model.addTube(helper.createUuid(0), barcodePresenter);	
-          });
+          model.addTube(helper.createUuid(0), barcodePresenter);
+        });
 
-        waitsFor(function() {
+        waitsFor(function () {
           return haveMutated;
-          }, "mutator to run", 100);
+        }, "mutator to run", 100);
 
-        runs(function() {
+        runs(function () {
           console.log("expecting model batch");
-	  expect(model.batch).toEqual(firstBatchUuid);
-          });
+          expect(model.batch).toEqual(firstBatchUuid);
+        });
       });
 
-      it("adding tube with no batch is handled gracefully", function() {
-	// TODO
-	nextBatchUuid = '';
-	model.addTube(helper.createUuid(0));
-	expect(model.batch).toBeUndefined();
-	});
+      it("adding tube with no batch is handled gracefully", function () {
+        // TODO
+        nextBatchUuid = '';
+        model.addTube(helper.createUuid(0));
+        expect(model.batch).toBeUndefined();
+      });
 
     });
 
-    describe("model with 1 tube", function() {
+    describe("model with 1 tube", function () {
       var model;
 
-      beforeEach(function() { 
-	createSpyOwner();
+      beforeEach(function () {
+        createSpyOwner();
         model = new SelectionPageModel(owner, 123456789);
         model.tubes.push(helper.createTubeWithOriginalBatch(0));
-	model.batch = firstBatchUuid;
-	haveMutated = false;
+        model.batch = firstBatchUuid;
+        haveMutated = false;
       });
 
-      it("contains one tube", function() { 
+      it("contains one tube", function () {
         expect(model.getNumberOfTubes()).toEqual(1);
       });
 
       it("adding new tube in same batch works fine", function () {
 
-	runs(function() {
-	  haveMutated = false;
-	  nextBatchUuid = firstBatchUuid;
+        runs(function () {
+          haveMutated = false;
+          nextBatchUuid = firstBatchUuid;
           var uuid = helper.createUuid(1);
-	  // Set up override uuid
-	  nextObjectUuid = uuid;
+          // Set up override uuid
+          nextObjectUuid = uuid;
           model.addTube(uuid);
-	});
-	waitsFor(function() {
-	  return haveMutated;
-	},
-		 "2 tubes to be created",
-		 50);	
-	runs( function() {
+        });
+        waitsFor(function () {
+              return haveMutated;
+            },
+            "2 tubes to be created",
+            50);
+        runs(function () {
           expect(model.getNumberOfTubes()).toEqual(2);
           expect(model.tubes[0].rawJson.tube.uuid).not.toEqual(model.tubes[1].rawJson.tube.uuid);
-	  });
+        });
       });
 
-      it("tube in different batch not added to model and sends error message to childDone", function() {	
-	runs(function() {
-	  nextBatchUuid = secondBatchUuid;
-	  model.addTube(helper.createUuid(1), barcodePresenter);	  
-	  });
-	waitsFor(function() {
-	  return haveMutated; },
-		"The mutator to override the batch uuid",
-		40);
-	runs(function() {
-	  expect(model.getNumberOfTubes()).toBe(1);
-	  expect(owner.childDone).toHaveBeenCalledWith(barcodePresenter, "error",
-						       {"type": "UuidMismatch", "message": "Tube in different batch to currently selected tubes."});
-	  });
+      it("tube in different batch not added to model and sends error message to childDone", function () {
+        runs(function () {
+          nextBatchUuid = secondBatchUuid;
+          model.addTube(helper.createUuid(1), barcodePresenter);
+        });
+        waitsFor(function () {
+              return haveMutated;
+            },
+            "The mutator to override the batch uuid",
+            40);
+        runs(function () {
+          expect(model.getNumberOfTubes()).toBe(1);
+          expect(owner.childDone).toHaveBeenCalledWith(barcodePresenter, "error",
+              {"type":"UuidMismatch", "message":"Tube in different batch to currently selected tubes."});
+        });
       });
 
-      it("removing last tube causes batch to be undefined", function() {
+      it("removing last tube causes batch to be undefined", function () {
         var uuid = helper.getUuidFromTube(model.tubes[0]);
         model.removeTubeByUuid(uuid);
         expect(model.getNumberOfTubes()).toEqual(0);
         expect(model.batch).toBeUndefined();
       });
 
-      it("attempting to add same tube again sends error message to parent", function() { 
-	runs(function() {
-	  nextBatchUuid = firstBatchUuid;
-	  nextObjectUuid = helper.createUuid(0);
-	  model.addTube(helper.createUuid(0), barcodePresenter);	  
-	  });
-	waitsFor(function() {
-	  return haveMutated;
-	  }, 
-		 "The mutator to have overriden the batch id",
-		 40);
-	runs(function() {
-	  expect(model.getNumberOfTubes()).toBe(1);
-	  expect(owner.childDone).toHaveBeenCalledWith(barcodePresenter, "error",
-						       {"type": "UuidMismatch", "message": "This tube has already been scanned."});
-	  });
-	
+      it("attempting to add same tube again sends error message to parent", function () {
+        runs(function () {
+          nextBatchUuid = firstBatchUuid;
+          nextObjectUuid = helper.createUuid(0);
+          model.addTube(helper.createUuid(0), barcodePresenter);
+        });
+        waitsFor(function () {
+              return haveMutated;
+            },
+            "The mutator to have overriden the batch id",
+            40);
+        runs(function () {
+          expect(model.getNumberOfTubes()).toBe(1);
+          expect(owner.childDone).toHaveBeenCalledWith(barcodePresenter, "error",
+              {"type":"UuidMismatch", "message":"This tube has already been scanned."});
+        });
+
       });
     });
 
-    describe("model with 12 tubes", function() {
+    describe("model with 12 tubes", function () {
       var model;
 
-      beforeEach(function() { 
-	createSpyOwner();
+      beforeEach(function () {
+        createSpyOwner();
         model = new SelectionPageModel(owner, 123456789);
-        for(var i = 0; i < 12; i++) {
+        for (var i = 0; i < 12; i++) {
           var newTube = helper.createTubeWithOriginalBatch(i);
-          
+
           model.tubes.push(newTube);
         }
       });
 
-      it("contains 12 tubes", function() { 
+      it("contains 12 tubes", function () {
         expect(model.getNumberOfTubes()).toEqual(12);
       });
 
-      it("remove tubes removes tube order and leaves batch defined", function() {
+      it("remove tubes removes tube order and leaves batch defined", function () {
         var uuid = helper.getUuidFromTube(model.tubes[5]);
         var originalBatch = model.batch;
         model.removeTubeByUuid(uuid);
         expect(model.getNumberOfTubes()).toEqual(11);
-        for(var i = 0; i < 11; i++) {
+        for (var i = 0; i < 11; i++) {
           var tube = model.tubes[i];
           expect(tube).toBeDefined();
-          expect(helper.getUuidFromTube(tube)).not.toEqual(uuid);	
+          expect(helper.getUuidFromTube(tube)).not.toEqual(uuid);
         }
 
         expect(model.batch).toEqual(originalBatch);
 
       });
 
-      it("attempting to remove an tube with no matching uuid leaves model unchanged", function() {
+      it("attempting to remove an tube with no matching uuid leaves model unchanged", function () {
         var uuid = helper.createUuid(20);
         model.removeTubeByUuid(uuid);
 
         expect(model.getNumberOfTubes()).toEqual(12);
       });
 
-      it("adding new tube in different batch throws exception", function() {
+      it("adding new tube in different batch throws exception", function () {
         var tube = helper.createTubeWithDifferentBatch(12);
-        expect(function() { model.addTube(order); }).toThrow();
+        expect(function () {
+          model.addTube(order);
+        }).toThrow();
       });
 
-      it("adding new tube in same batch throws exception", function() {
+      it("adding new tube in same batch throws exception", function () {
         var tube = helper.createTubeWithOriginalBatch(12);
-        expect(function() { model.addTube(tube); }).toThrow();
+        expect(function () {
+          model.addTube(tube);
+        }).toThrow();
       });
 
-      it("can return uuid from tube index", function() {
-        for(var i = 0; i < model.getNumberOfTubes(); i++) {
+      it("can return uuid from tube index", function () {
+        for (var i = 0; i < model.getNumberOfTubes(); i++) {
           var expectedUuid = helper.createUuid(i);
           expect(model.getTubeUuidFromTubeIndex(i)).toBe(expectedUuid);
         }
       });
-    })});
+    })
+  });
 
 });
