9b606ea348ed37801f7fe0932a408ceb0b6f1e43
David Allen <da6@sanger.ac.uk>
2013-04-02 13:33:26 +0100

    Refactoring on elution loading and elution wash views

--- app/scripts/views/elution_loading_page_view.js
+++ app/scripts/views/elution_loading_page_view.js
@@ -1,4 +1,4 @@
-define([], function () {
+define(['text!extraction_pipeline/html_partials/elution_loading_partial.html'], function (elutionLoadingPartialHtml) {
 
   function getKey(e) {
     if (window.event) {
@@ -27,28 +27,11 @@ define([], function () {
     }
 
 
-    var parent = this.jquerySelector(),
-      htmlParts = [
-        '<h2 class="title">Loading Elution for Order: </h2>',
-        '<hr />',
-        '<div class="row0"></div>',
-        '<div class="row1"></div>',
-        '<div class="row2"></div>',
-        '<div class="row3"></div>',
-        '<div class="row4"></div>',
-        '<div class="row5"></div>',
-        '<div class="row6"></div>',
-        '<div class="row7"></div>',
-        '<div class="row8"></div>',
-        '<div class="row9"></div>',
-        '<div class="row10"></div>',
-        '<div class="row11"></div>',
-        '<p align="right"><button class="btn printButton">Print Output Tube Barcodes</button><button class="btn startButton">Start Elution process</button></p>'],
-      htmlString = htmlParts.join('');
+    var parent = this.jquerySelector();
 
     // We have to append to the document or events won't register
     parent.empty().
-      append(htmlString);
+      append(elutionLoadingPartialHtml);
     var startButton = parent.find(".startButton");
     var that = this;
 
