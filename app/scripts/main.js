edf116b3df69decc31c9f7193231a95bb08a5d06
benoitmangili <benoit.mangili@tessella.com>
2013-04-05 14:16:48 +0100

    restored the config file +
    Merge branch 'Elution_Page_Functionality' of https://github.com/mikemases/s2_extraction_pipeline into working_with_s2_instance
    
    Conflicts:
    	app/scripts/dna_and_rna_manual_extraction.json
    	app/scripts/models/kit_binding_model.js
    	app/scripts/presenters/kit_binding_page_presenter.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,51 +24,8 @@ require(['extraction_pipeline/app',
   function (app, PresenterFactory) {
     var theApp = new app(new PresenterFactory());
     var inputModelForApp = undefined;
-//  inputModelForApp = {
-//    userUUID:"2345678901234",
-//    labwareUUID:"106d61c0-6224-0130-90b6-282066132de2",
-//    batchUUID:"1234567890"
-//  };
-       theApp.tubeUUIDs =  [
-        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"},
-        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"},
-        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"},
-        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"}
-      ];
 
     theApp.setupPresenter(inputModelForApp);
-
-
-//    var kitPresenter = new KitPresenter(undefined, new pf());
-//    var batchPresenter = new BatchPresenter(undefined, new pf());
-//    var dummyInput = {
-//      "tubes" : [
-//        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"},
-//        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"},
-//        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"},
-//        {"uuid" : "106d61c0-6224-0130-90b6-282066132de2"}
-//      ]
-//    };
-
-//    kitPresenter.setupPresenter(dummyInput, function() { return $("#content"); });
-//    batchPresenter.setupPresenter(dummyInput, function() { return $("#content"); });
-//    var lp = new LabwarePresenter(undefined, new pf());
-//    lp.setupPresenter(undefined, function() { return $("#content"); });
-
-//  var labwarePresenter = new LabwarePresenter(undefined, new pf());
-/// var labwareModel = {"uuid" : "106d61c0-6224-0130-90b6-282066132de2",
-
-
-//                      "expected_type" : "tube"};
-//  labwarePresenter.setupPresenter(labwareModel, function() { return $("#content"); })
-//
-//  var selectionPagePresenter = new SelectionPagePresenter(theApp, new pf());
-//  var selectionPageModel = {userUUID:"1234567890", labwareUUID:"106d61c0-6224-0130-90b6-282066132de2", batchUUID:"0123456789"};
-//  selectionPagePresenter.setupPresenter(selectionPageModel, function() {return $("#content"); });
-
-//    "expected_type" : "tube"};
-//  labwarePresenter.setupPresenter(labwareModel, function() { return $("#content"); })
-
 
 });
 
