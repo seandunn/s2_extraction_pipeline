d7d18ff49752d839b74f0133d61ef859f6444c1b
Michael Mases <mm22@sanger.ac.uk>
2013-04-23 16:03:45 +0100

    Cleaned up default page code

--- app/scripts/default/default_model.js
+++ app/scripts/default/default_model.js
@@ -1,42 +1,16 @@
-/*
- * S2 - An open source lab information management systems (LIMS)
- * Copyright (C) 2013  Wellcome Trust Sanger Insitute
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 1, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
- */
-
 define([
   'extraction_pipeline/models/base_page_model'
 ], function (BasePageModel) {
-
+  'use strict';
 
   var DefaultPageModel = Object.create(BasePageModel);
 
 
   $.extend(DefaultPageModel, {
     init:function (owner) {
-//      BasePageModel.init(owner);
       this.owner = Object.create(owner);
       this.stash_by_BC = {};
       this.stash_by_UUID = {};
-
-
-
-      this.labware = undefined;
-      this.user = undefined;
-      this.batch = undefined;
       return this;
     },
     setLabware:function (rsc) {
