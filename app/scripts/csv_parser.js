6c18b6150e08771276155a864f7c7a3a57f962f3
benoitmangili <benoit.mangili@tessella.com>
2013-04-23 21:15:42 +0100

    adding the csv parser module (partial implementation)

--- app/scripts/csv_parser.js
+++ app/scripts/csv_parser.js
@@ -1,43 +1,88 @@
-define([], function() {
+define([], function () {
   'use strict';
   return {
-    csvToArray:function(data){
+    csvToArray:function (data) {
       var delimiter = ',';
 
       var lines = data.split(/\r\n|\r|\n/g);
 
       var re = new RegExp(
-          "\\s*(\\w+\\d+)\\s*\\"+delimiter
-          +
-          "\\s*\\\"(\\w+)\\\"","i")
-
-
+          "\\s*(\\w+\\d+)\\s*\\" + delimiter
+              +
+              "\\s*\\\"(FR\\w+)\\\"", "i")
 
       var array = [];
       var matches = undefined;
 
-      _.each(lines,function(line){
+      _.each(lines, function (line) {
         matches = re.exec(line);
         var location = matches[1];
         var barcode = matches[2];
-        array.push([location,barcode]);
+        array.push([location, barcode]);
       });
 
       return array;
     },
-    locationArrayToLocalisedResources:function(array){
-      _.each(array,function(row){
-        _.each(row,function(col){
-          if (col === "MISREAD"){
-            throw "one row is malformed : "+row
+    locationArrayToLocalisedBarcodes:function (array) {
+      var output = {};
+      _.each(array, function (row) {
+        var location = row[0]
+        var barcode = row[1]
+        if (barcode === "Not read") {
+          // does nothing
+        } else {
+          output[location] = {
+            "barcode":barcode
           }
-        });
+        }
       });
 
-      return {};
+      return output;
     },
-    convertCSVDataToJSON:function(data){
-      return this.locationArrayToLocalisedResources(this.csvToArray(data));
+    getRequestParamForMultipleBarcodeSearch:function (root,barcodeHash) {
+      var parser = this;
+      var wholePromise = $.Deferred();
+      var dataArray = {};
+
+      var barcodes = _.reduce(barcodeHash,function(memo,barcode){
+        return memo.push(barcode);
+      });
+
+
+
+//      var array = _.map(barcodeHash, function (details, location) {
+//        var promise = $.Deferred();
+//        parser.getUUIDsByBarcodesPromise(root, details.barcode)
+//            .then(function (uuid) {
+//              dataArray[location]={
+//                "barcode":details.barcode,
+//                "uuid":uuid
+//              };
+//              promise.resolve();
+//            }).fail(function () {
+//              promise.reject();
+//            });
+//
+//        return promise.promise();
+//      });
+//
+//      $.when.apply(null,array).then(function(){
+//        wholePromise.resolve(dataArray);
+//      }).fail(wholePromise.reject);
+
+      return wholePromise.promise();
+    },
+    getUUIDsByBarcodesPromise:function (root, barcodes) {
+
+
+//      return root.labellables.findByEan13Barcode(barcodes)
+//          .then(function (label) {
+//            return label.name;
+//          });
+
+    },
+    convertCSVDataToJSON:function (data) {
+      return this.locationArrayToLocalisedBarcodes(this.csvToArray(data));
     }
 
   };
