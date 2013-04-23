952d114b9d5a7e960a3bdeff02f51f4c9beb265c
David Allen <da6@sanger.ac.uk>
2013-04-23 16:23:58 +0100

    update to selection page presenter spec (partial)

--- test/spec/selection_page_presenter_spec.js
+++ test/spec/selection_page_presenter_spec.js
@@ -1,255 +1,221 @@
 define([
   'config'
+  , 'mapper_test/resource_test_helper'
+  , 'mapper/s2_root'
   , 'models/selection_page_model'
   , 'presenters/selection_page_presenter'
   , 'presenters/presenter_factory'
   , 'text!testjson/unit/root.json'
   , 'text!extraction_pipeline/dna_and_ran_manual_test_data.json'
-], function (config, SelectionPageModel, SelectionPagePresenter, PresenterFactory, rootTestData, testData) {
+], function (config, TestHelper, S2Root, SelectionPageModel, SelectionPagePresenter, PresenterFactory, rootTestData, testData) {
   'use strict';
 
-  describe("Selection page presenter", function () {
 
-
-    describe(" which has spy subpresenters", function () {
-
-      beforeEach(function () {
-
-        config.loadTestData(testData);
-        config.cummulativeLoadingTestDataInFirstStage(rootTestData);
-        config.logLevel = 0;
-
-        // create a mock of the app
-        var app = {};
-        app.childDone = function (presenter, action, data) {
-        };
-
-        // create a mock of the view
-        var view = {};
-        view.clear = function () {
-        };
-        view.render = function (data) {
-        };
-        view.getRowByIndex = function (index) {
-          return "row_" + index;
-        };
-
-        //spy on the mock view functions
-        spyOn(view, 'clear');
-        spyOn(view, 'render');
-
-        var presenter = PresenterFactory.create(presenterName, this.mainController, initData);
-
-        var configLoaded = false;
-
-        // Iterate through the data to a point where two tubes are created
-        runs(function(){
-          for (var i=1; i<10; i++){
-            config.progress(String(i));
-          }
-          var hasConfigLoaded = true;
-        });
-        waitsFor(function(){
-          return configLoaded;
-        });
-
-
-        presenter.setupPresenter(model, function () {
-          return $("#content");
-        });
-        presenter.model = model;
-        presenter.setupSubPresenters();
-        // setup presenter sets the view, we now want to override it to be
-        // a the test spy view
-        presenter.view = view;
+  function getAResource(owner, uuid) {
+    var deferredS2Resource = new $.Deferred();
+    owner.getS2Root()
+      .then(function (root) {
+        return root.find(uuid);
+      }).then(function (result) {
+        deferredS2Resource.resolve(result);
+      }).fail(function () {
+        deferredS2Resource.reject();
       });
+    return deferredS2Resource.promise();
+  }
 
-      it("kjh",function(){
-        runs(function(){
-          expect(1).toEqual(7);
+
+  TestHelper(function (results) {
+
+
+    describe("Selection page presenter", function () {
+
+      var s2Root = undefined;
+
+
+      describe(" which is given one tube", function () {
+
+        var presenter;
+
+        beforeEach(function () {
+
+          config.loadTestData(testData);
+          config.cummulativeLoadingTestDataInFirstStage(rootTestData);
+          config.logLevel = 0;
+
+          var app = {
+            getS2Root:function () {
+              var deferredS2Root = new $.Deferred();
+              if (!s2Root) {
+                S2Root.load({user:"username"}).done(function (result) {
+                  s2Root = result;
+                  deferredS2Root.resolve(result);
+                }).fail(function () {
+                    deferredS2Root.reject();
+                  });
+              } else {
+                deferredS2Root.resolve(s2Root);
+              }
+              return deferredS2Root.promise();
+            },
+            childDone:function () {
+            }
+          };
+
+
+          var presenterName = "selection_page_presenter";
+          var initData = {
+            "presenterName":"selection_page_presenter",
+            "processTitle": "Manual DNA and RNA Extraction",
+            "input":        {
+              "role":"samples.extraction.manual.dna_and_rna.input_tube_nap"
+            },
+            "output":       [
+              {
+                "role":       "samples.extraction.manual.dna_and_rna.binding_input_tube_nap",
+                "aliquotType":"NA+P"
+              }
+            ]
+          };
+
+          var pf = new PresenterFactory();
+          presenter = pf.create(presenterName, this.mainController, initData);
+
+          var model, initialLabware;
+
+          runs(function () {
+            app.getS2Root().then(function (root) {
+              return root.tubes.findByEan13Barcode("1220017279667");
+            }).then(function (tube) {
+                initialLabware = tube;
+
+                // Iterate through to where a tube is retrieved
+                for (var i = 1; i < 2; i++) {
+                  config.progress(String(i));
+                }
+
+                model = {
+                  userUUID:"123456789",
+                  labware: initialLabware,
+                  batch:   undefined
+                }
+
+              })
+              .then(results.expected)
+              .fail(results.unexpected)
+          });
+
+          waitsFor(results.hasFinished);
+
+
+          runs(function () {
+            results.resetFinishedFlag();
+            presenter.setupPresenter(model, function () {
+              return $("#content");
+            });
+            spyOn(presenter.currentView, "render");
+            spyOn(presenter.currentView, "attachEvents");
+            spyOn(presenter.currentView, "clear");
+          });
         });
+
+        it("is defined", function () {
+          runs(function () {
+            expect(presenter).toBeDefined();
+          });
+        });
+
+        it("has a view that has been set up correctly", function () {
+          runs(function () {
+            expect(presenter.currentView).toBeDefined();
+            expect(presenter.currentView.owner).toEqual(presenter);
+            expect(presenter.currentView.jquerySelector).toBeDefined();
+          });
+        });
+
+        it("calls the render method in the view when render is called in the presenter", function () {
+          runs(function () {
+            var expectedData = {
+              batch:       undefined,
+              user:        '123456789',
+              capacity:    12,
+              processTitle:'Manual DNA and RNA Extraction'
+            };
+
+            presenter.renderView();
+
+            expect(presenter.currentView.render).toHaveBeenCalledWith(expectedData);
+            expect(presenter.currentView.render.callCount).toEqual(1);
+          });
+        });
+
+        it("has 12 sub presenters that are labware presenters", function () {
+          runs(function () {
+            expect(presenter.presenters.length).toEqual(12);
+            _.each(presenter.presenters, function (subPresenter) {
+              expect(subPresenter).toBeDefined();
+              expect(subPresenter.labwareModel).toBeDefined();
+              expect(subPresenter.view).toBeDefined();
+            });
+          });
+        });
+
+        it("the first sub presenter has a tube presenter", function () {
+          var subPresenters = presenter.presenters;
+          runs(function () {
+            expect(subPresenters[0].resourcePresenter).toBeDefined();
+            expect(subPresenters[0].labwareModel.display_barcode).toEqual(false);
+            expect(subPresenters[0].labwareModel.display_remove).toEqual(true);
+          });
+        });
+
+        it("the second sub presenter has a scan barcode presenter", function () {
+
+          runs(function () {
+            var subPresenters = presenter.presenters;
+            expect(subPresenters[1].barcodeInputPresenter).toBeDefined();
+            expect(subPresenters[1].labwareModel.display_barcode).toEqual(true);
+            expect(subPresenters[1].labwareModel.display_remove).toEqual(false);
+          });
+        });
+
+        it("the remaining sub presenters don't have tube or scan barcode presenters", function () {
+          runs(function () {
+            var subPresenters = presenter.presenters;
+            _.chain(subPresenters)
+              .drop(2)
+              .each(function (subPresenter) {
+                expect(subPresenter.labwareModel.display_barcode).toEqual(false);
+                expect(subPresenter.labwareModel.display_remove).toEqual(false);
+                expect(subPresenter.resourcePresenter).not.toBeDefined();
+                expect(subPresenter.barcodeInputPresenter).not.toBeDefined();
+              });
+          });
+        });
+
+        it("displays a barcode error on the correct presenter", function(){
+          runs(function(){
+            var errorMessage = "Generic error message";
+            spyOn(presenter.presenters[1], "displayErrorMessage");
+            presenter.displayBarcodeError(errorMessage);
+            expect(presenter.presenters[1].displayErrorMessage).toHaveBeenCalledWith(errorMessage);
+          })
+        });
+
+        it("calls the clear method of the view when release is called in the presenter", function(){
+          runs(function(){
+            presenter.release();
+            expect(presenter.currentView.clear).toHaveBeenCalled();
+          });
+        });
+
+        it("Child done stuff",function(){
+          runs(function(){
+
+          });
+        });
+
       });
-
-      /*
-
-       var model = undefined;
-       var presenter = undefined;
-       var view = undefined;
-       var app = undefined;
-       var partialFactory = undefined;
-       var mockPresenters = undefined;
-       var mockViews = undefined;
-
-       function configureSpyView() {
-       view = {};
-       view.clear = function () {
-       };
-       view.render = function (data) {
-       };
-       view.getRowByIndex = function (index) {
-       return "row_" + index;
-       };
-
-       spyOn(view, 'clear');
-       spyOn(view, 'render');
-       }
-
-       function configureSpyAppController() {
-       app = {};
-       app.childDone = function (presenter, action, data) {
-       };
-
-       spyOn(app, 'childDone');
-       }
-
-       function createMockPresenter(name) {
-       var mockPresenter = { name:name};
-       mockPresenter.setupPresenter = function (input_model, jquerySelection) {
-       };
-       mockPresenter.setupPlaceholder = function (jquerySelection) {
-       };
-       mockPresenter.updateModel = function (input_model) {
-       };
-       mockPresenter.setupSubModel = function (model, jquerySelection) {
-       };
-       mockPresenter.setupView = function () {
-       };
-       mockPresenter.renderView = function () {
-       };
-       mockPresenter.release = function () {
-       };
-       mockPresenter.childDone = function (presenter, action, data) {
-       };
-
-       spyOn(mockPresenter, 'setupPresenter');
-       spyOn(mockPresenter, 'setupPlaceholder');
-       spyOn(mockPresenter, 'setupSubModel');
-       spyOn(mockPresenter, 'setupView');
-       spyOn(mockPresenter, 'renderView');
-       spyOn(mockPresenter, 'release');
-       spyOn(mockPresenter, 'childDone');
-
-       mockPresenters.push(mockPresenter);
-       return mockPresenter;
-       }
-
-       var createMockView = function(name) {
-       var mockView = Object.create(null);
-       mockView.name = name;
-
-       mockView.render = function(model){
-       };
-       mockView.attachEvents = function(){
-       };
-
-       spyOn(mockView, 'render');
-       spyOn(mockView, 'attachEvents')
-       return mockView;
-       };
-
-       function configureMockPartialFactory() {
-       partialFactory = {};
-       partialFactory.createPresenter = function (name, owner) {
-       return createMockPresenter(name);
-       };
-       partialFactory.createScanBarcodePresenter = function (owner, type) {
-       var presenter = this.createPresenter("scanBarcode", owner);
-       presenter.setupPresenter(type);
-       return presenter;
-       };
-       partialFactory.createTubeRemovalPresenter = function (owner) {
-       return this.createPresenter("tubeRemoval", owner);
-       }
-       }
-
-       function expectPartial(partial, typeName, args) {
-       expect(partial).toBeDefined();
-       expect(partial.name).toEqual(typeName);
-       if (args) {
-       expect(partial.setupPresenter).toHaveBeenCalledWith(args);
-       }
-       expect(partial.renderView).toHaveBeenCalled();
-       }
-
-
-       */
-
-
-
-
-
-
-//      it("updating presenter with empty model creates a ScanBarcodePresenter", function () {
-//        runs(function () {
-//          // one scan barcode and one tube removal
-//          expect(mockPresenters.length).toBe(2);
-//          presenter.renderView();
-//        });
-//        waitsFor(function () {
-//              return mockPresenters.length >= 2;
-//            },
-//            "child presenters were never created",
-//            100);
-//        runs(function () {
-//          expect(view.render).toHaveBeenCalled();
-//          expect(mockPresenters.length).toBeGreaterThan(1);
-//          expectPartial(mockPresenters[0], "tubeRemoval", null);
-//          expectPartial(mockPresenters[1], "scanBarcode", "tube");
-//        });
-//      });
-//
-//      it("presenter release calls clear", function () {
-//        presenter.release();
-//        expect(view.clear).toHaveBeenCalled();
-//        expect(view.render).not.toHaveBeenCalled();
-//      });
-//
-//      it("childDone on next command delegates to app controller", function () {
-//        presenter.childDone(presenter, "next", undefined);
-//        expect(app.childDone).toHaveBeenCalledWith(presenter, "done", undefined);
-//      });
-//
-//      it("removeTube message with uuid not matching any tube does nothing", function () {
-//        runs(function () {
-//          presenter.renderView();
-//        });
-//        waitsFor(function () {
-//              return mockPresenters.length >= 2;
-//            },
-//            "2 child presenters to be created",
-//            100);
-//        runs(function () {
-//          var tubeCount = model.getNumberOfTubes();
-//          presenter.childDone(presenter, "removeTube", { tube:{ uuid:"1" } });
-//          expect(model.getNumberOfTubes()).toBe(tubeCount);
-//        });
-//      });
-//
-//      it("removeTube message with uuid matching tube removes tube by uuid", function () {
-//        var tubeCount;
-//        runs(function () {
-//          presenter.renderView();
-//        });
-//        waitsFor(function () {
-//              return model.getNumberOfTubes() === 1
-//            },
-//            "tube to be added",
-//            1000);
-//        runs(function () {
-//          tubeCount = model.getNumberOfTubes();
-//          expect(tubeCount).toBe(1);
-//          presenter.childDone(this, "removeTube", { tube:{ uuid:"11111111-2222-3333-4444-0000000000000" } });
-//        });
-//        waitsFor(function () {
-//              return (model.getNumberOfTubes() === 0);
-//            },
-//            "tube count to be decremented " + model.getNumberOfTubes(),
-//            100);
-//        runs(function () {
-//          expect(model.getNumberOfTubes()).toBe(0);
-//        });
-//
-//      });
     });
   });
 });
