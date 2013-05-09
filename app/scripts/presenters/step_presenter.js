b18dcb357b8a334553aec38a5dea694df74de69c
Michael Mases <mm22@sanger.ac.uk>
2013-05-09 14:13:17 +0100

    Corrections following comments

--- app/scripts/presenters/step_presenter.js
+++ app/scripts/presenters/step_presenter.js
@@ -1,21 +1,21 @@
 define([
-       'extraction_pipeline/presenters/base_presenter',
-       'extraction_pipeline/views/step_view'
-], function(Base, View) {
+  'extraction_pipeline/presenters/base_presenter',
+  'extraction_pipeline/views/step_view'
+], function (Base, View) {
   'use strict';
 
   var Presenter = Object.create(Base);
 
   _.extend(Presenter, {
-    register: function(callback) {
-      callback('step_presenter', function() {
+    register:function (callback) {
+      callback('step_presenter', function () {
         var instance = Object.create(Presenter);
         Presenter.init.apply(instance, arguments);
         return instance;
       });
     },
 
-    init: function(owner, factory, config) {
+    init:function (owner, factory, config) {
       this.owner = owner;
       this.config = config;
       this.factory = factory;
@@ -23,15 +23,15 @@ define([
       this.config.buttons = this.config.buttons || [
         {action:"print", title:"Print labels"  },
         {action:"start", title:"Start process" },
-        {action:"end",   title:"End process"   },
-        {action:"next",  title:"Next"          }
+        {action:"end", title:"End process"   },
+        {action:"next", title:"Next"          }
       ];
 
       var presenter = this;
       return this;
     },
 
-    setupPresenter: function(model, selector) {
+    setupPresenter:function (model, selector) {
       var presenter = this;
       this.selector = selector;
       this.batch    = model.batch;
@@ -42,15 +42,17 @@ define([
       this.setupSubPresenters();
       return this;
     },
-    setupSubPresenters: function() {
+    setupSubPresenters:function () {
       var presenter = this;
-      presenter.presenters = _.chain(presenter.config.presenters).map(function(config, index) {
+      presenter.presenters = _.chain(presenter.config.presenters).map(function (config, index) {
         var subPresenter = presenter.factory.create(config.presenterName, presenter, config);
         subPresenter.setupPresenter({
-          batch: presenter.batch
-        }, (function(i) {
-          return function() { return presenter.selector().find('#step'+i); }
-        })(index+1));
+          batch:presenter.batch
+        }, (function (i) {
+          return function () {
+            return presenter.selector().find('#step' + i);
+          }
+        })(index + 1));
 
         return subPresenter;
       }).value();
@@ -59,44 +61,42 @@ define([
       presenter.view.selectPrinter(presenter.activePresenter.config.defaultPrinter);
       presenter.activePresenter.focus();
     },
-    setupSubModel: function() {
+    setupSubModel:function () {
       return this;
     },
 
-    setupView: function() {
+    setupView:function () {
       this.view = new View(this, this.selector);
       this.view.setPrinterList(this.printerList());
       return this;
     },
-    release: function() {
+    release:function () {
       this.view.clear();
       return this;
     },
-    renderView: function() {
+    renderView:function () {
 
       this.view.renderView({
-        user: this.user,
-        processTitle: this.config.processTitle,
+        user:this.user,
+        processTitle:this.config.processTitle,
         buttons:this.config.buttons
       });
       return this;
     },
 
-    childDone: function(child, action, data) {
+    childDone:function (child, action, data) {
       var presenter = this;
       var btnDetailsList;
 
-      if (child === this.view)
-      {
+      if (child === this.view) {
         var handler = this.activePresenter[action];
         handler && handler.apply(this.activePresenter, arguments);
       }
-      else if (action === 'done')
-      {
+      else if (action === 'done') {
         var index = _.indexOf(this.presenters, child);
         if (index !== -1) {
-          var activeSubPresenter = presenter.presenters[index+1] || {
-            previousDone:function() {
+          var activeSubPresenter = presenter.presenters[index + 1] || {
+            previousDone:function () {
               presenter.owner.childDone.apply(presenter.owner, arguments);
             }
           };
@@ -106,42 +106,28 @@ define([
           presenter.view.selectPrinter(presenter.activePresenter.config.defaultPrinter);
           presenter.activePresenter.focus();
         }
-      }
-      else if (action === 'enableBtn' || action === 'disableBtn')
-      {
+      } else if (action === 'enableBtn' || action === 'disableBtn') {
         btnDetailsList = data.actions || this.config.buttons;
-        _.each(btnDetailsList, function(btnDetails){
+        _.each(btnDetailsList, function (btnDetails) {
           presenter.view.setButtonEnabled(btnDetails.action, action === 'enableBtn');
         })
       }
-      else if (action === 'showBtn' || action === 'hideBtn')
-      {
-        presenter.changeButonsVisibility(action,data);
+      else if (action === 'showBtn' || action === 'hideBtn') {
+        presenter.changeButtonsVisibility(action, data);
       }
-      else
-      {
+      else {
         this.owner.childDone(child, action, data);
       }
     },
 
-    changeButonsVisibility:function(action, data){
+    changeButtonsVisibility:function (action, data) {
       var btnDetailsList;
       var presenter = this;
       btnDetailsList = data.actions || this.config.buttons;
-      _.each(btnDetailsList, function(btnDetails){
-        presenter.view.setButtonVisible(btnDetails.action, action === 'showBtn');
-      })
-    },
-
-    changeButonsStatus:function(action, data){
-      var btnDetailsList;
-      var presenter = this;
-      btnDetailsList = data.actions || this.config.buttons;
-      _.each(btnDetailsList, function(btnDetails){
+      _.each(btnDetailsList, function (btnDetails) {
         presenter.view.setButtonVisible(btnDetails.action, action === 'showBtn');
       })
     }
-
   });
 
   return Presenter;
