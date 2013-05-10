16f32ed4fe5dfd284f257172478b8c1ff68327db
benoitmangili <benoit.mangili@tessella.com>
2013-05-10 14:38:07 +0100

    space correction

--- app/scripts/models/volume_check_model.js
+++ app/scripts/models/volume_check_model.js
@@ -107,7 +107,7 @@ define([
             model.owner.childDone(model, "error", {message: "couldn't load the batch resources!"});
           });
     },
-    setUser:     function (userUUID) {
+    setUser: function (userUUID) {
       this.user = userUUID;
       this.owner.childDone(this, "userAdded");
     }
