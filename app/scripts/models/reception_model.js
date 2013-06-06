284eefb1e36fcf4fce8c4eb47ceb501069ef3c5e
benoitmangili <benoit.mangili@tessella.com>
2013-06-06 12:53:13 +0100

    fixing missing sanger id in csv sent for merging

--- app/scripts/models/reception_model.js
+++ app/scripts/models/reception_model.js
@@ -161,14 +161,14 @@ define([
       return deferred.promise();
     },
 
-    generateCSVBlob: function(labellables){
+    generateCSVBlob: function(){
       var thisModel = this;
       var data = _.map(thisModel.outputs,function(labware){
         var sampleUUID = labware.aliquots[0].sample.uuid;
         var sanger_sample_id = _.find(thisModel.samples,function (sample) {
-          return d3_geo_resample.uuid === sampleUUID;
+          return sample.uuid === sampleUUID;
         }).sanger_sample_id;
-        return [labellable.labels.barcode.value, sanger_sample_id ].join(',');
+        return [labware.labels.barcode.value, sanger_sample_id ].join(',');
       });
       data.unshift("Tube Barcode,Sanger Sample ID");
       var txt = data.join("\n");
