248884f679bbce6813589840c9ddf9a9f3deb646
Sean Dunn <sd9@sanger.ac.uk>
2013-06-10 18:55:24 +0100

    Removed dead method

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,83 +5,75 @@ define([ 'config'
   , 'extraction_pipeline/extra_components/busy_box'
   , 'extraction_pipeline/alerts'
 ], function (config, WorkflowEngine, S2Root, workflowConfiguration, BusyBox, alerts) {
-      'use strict';
+  'use strict';
 
-      var App = function (thePresenterFactory) {
-        this.presenterFactory = thePresenterFactory;
-        this.workflowEngine = new WorkflowEngine(this, $.parseJSON(workflowConfiguration));
-        _.templateSettings.variable = 'templateData';
+  var App = function (thePresenterFactory) {
+    this.presenterFactory = thePresenterFactory;
+    this.workflowEngine = new WorkflowEngine(this, $.parseJSON(workflowConfiguration));
+    _.templateSettings.variable = 'templateData';
 
-        $('#server-url').text(config.apiUrl);
-        $('#release').text(config.release);
-        // ToDo #content exists at this point we should pass it directly not a function
-        this.jquerySelection = function () { return $('#content'); };
-        this.addEventHandlers();
-        return this;
-      };
+    $('#server-url').text(config.apiUrl);
+    $('#release').text(config.release);
+    // ToDo #content exists at this point we should pass it directly not a function
+    this.jquerySelection = function () { return $('#content'); };
+    this.addEventHandlers();
+    return this;
+  };
 
-      App.prototype.addEventHandlers = function(){
-        BusyBox.init();
-      };
+  App.prototype.addEventHandlers = function(){
+    BusyBox.init();
+  };
 
-      App.prototype.getS2Root = function() {
-        if (this.rootPromise === undefined) {
-          // User should be passed in here not hard-coded
-          this.rootPromise = S2Root.load({user:"username"});
-        }
+  App.prototype.getS2Root = function() {
+    if (this.rootPromise === undefined) {
+      // User should be passed in here not hard-coded
+      this.rootPromise = S2Root.load({user:"username"});
+    }
 
-        return this.rootPromise;
-      };
+    return this.rootPromise;
+  };
 
-      App.prototype.setupPresenter = function (inputModel) {
-        alerts.setupPlaceholder(function () {
-          return $('#alertContainer');
-        });
-        this.updateModel(inputModel || {});
+  App.prototype.setupPresenter = function (inputModel) {
+    alerts.setupPlaceholder(function () {
+      return $('#alertContainer');
+    });
+    this.updateModel(inputModel || {});
 
-        return this;
-      };
+    return this;
+  };
 
-      App.prototype.updateModel = function (model) {
-        var application = this;
-        this.model = $.extend(this.model, model);
+  App.prototype.updateModel = function (model) {
+    var application = this;
+    this.model = $.extend(this.model, model);
 
-        if (this.currentPagePresenter) {
-          this.currentPagePresenter.release();
-          delete this.currentPagePresenter;
-        }
+    if (this.currentPagePresenter) {
+      this.currentPagePresenter.release();
+      delete this.currentPagePresenter;
+    }
 
-        this.workflowEngine.nextPresenter(this.presenterFactory, this.model).
-          then(function(presenter){
-            application.setupNextPresenter(presenter);
-        });
+    this.workflowEngine.nextPresenter(this.presenterFactory, this.model).
+      then(function(nextPresenter){
+      application.currentPagePresenter = nextPresenter;
+      application.currentPagePresenter.setupPresenter(application.model, application.jquerySelection);
+      delete application.model.labware;
+    });
 
-        return this;
-      };
+    return this;
+  };
 
-      App.prototype.setupNextPresenter = function (nextPresenter) {
-        this.currentPagePresenter = nextPresenter;
-        this.currentPagePresenter.setupPresenter(this.model, this.jquerySelection);
-        delete this.model.labware;
-        return this;
-      };
+  App.prototype.childDone = function (child, action, data) {
+    console.log("A child of App (", child, ") said it has done the following action '" + action + "' with data :", data);
 
-      App.prototype.childDone = function (child, action, data) {
-        console.log("A child of App (", child, ") said it has done the following action '" + action + "' with data :", data);
+    var application = this;
+    config.exceptionHandling(function() {
+      if (action == "done") {
+        application.updateModel(data);
+      } else if (action == "login") {
+        application.updateModel(data);
+      }
+    });
+    return application;
+  };
 
-        var application = this;
-        config.exceptionHandling(function() {
-          if (action == "done") {
-            $('html, body').animate({scrollTop:0}, 'slow');
-            application.updateModel(data);
-          } else if (action == "login") {
-            application.updateModel(data);
-          } else if (action == "foundNextPresenter") {
-            application.setupNextPresenter(data);
-          }
-        });
-        return application;
-      };
-
-      return App;
+  return App;
 });
