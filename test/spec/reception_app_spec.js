411a2f6c193fe28c9aa6fe433accfd8cc4576fc6
David Allen <da6@sanger.ac.uk>
2013-06-11 15:34:37 +0100

    tests added for reception app (partial)

--- test/spec/reception_app_spec.js
+++ test/spec/reception_app_spec.js
@@ -7,8 +7,8 @@ define([
   , 'text!mapper_testjson/unit/root.json'
   , 'mapper/s2_root'
   , 'text!pipeline_testcsv/manifest_csv_test_data.csv'
-]
-  , function (config, FakeUser, appTestData, TestHelper, ReceptionPresenter, rootTestData, S2Root, manifestCSVData) {
+  , 'presenters/presenter_factory']
+  , function (config, FakeUser, appTestData, TestHelper, ReceptionPresenter, rootTestData, S2Root, manifestCSVData, PresenterFactory) {
     'use strict';
 
 
@@ -17,83 +17,129 @@ define([
 
         var presenter, fakeDOM, fakeContent;
         beforeEach(function () {
-
           config.loadTestData(appTestData);
           config.cummulativeLoadingTestDataInFirstStage(rootTestData);
           var app;
-          fakeDOM = $('<div><div id="content"/></div>');
+          fakeDOM = $('<div><div id="content"></div></div>');
           fakeContent = function () {
             return fakeDOM.find("#content");
           };
-
           app = {
             getS2Root: function () {
               if (this.rootPromise === undefined) {
-                // User should be passed in here not hard-coded
                 this.rootPromise = S2Root.load({user: "username"});
               }
               return this.rootPromise;
             }
           };
-
+          var pf = new PresenterFactory();
           var presenterConfig = {};
-          presenter = Object.create(ReceptionPresenter).init(app, config);
+          presenter = pf.create('reception_presenter', app, presenterConfig);
           fakeContent().append(presenter.view);
         });
 
-
         it('gives the user the option to create or load a manifest', function () {
-          var headers = fakeContent().find('h4');
-
-          expect(_.find(headers, function (header) {
-            return header.innerHTML === "Create a new manifest";
-          })).toBeTruthy();
-
-          expect(_.find(headers, function (header) {
-            return header.innerHTML === "Load a manifest";
-          })).toBeTruthy();
-
+          expect(fakeContent().find('#create-manifest-btn').length).toEqual(1);
+          expect(fakeContent().find('#read-manifest-btn').length).toEqual(1);
         });
 
-        describe("and a manifest csv is loaded and registered", function () {
-          var expectedData;
-          it("makes the correct call", function () {
-            //trigger the file input directly with a string, can't do this from view
+        describe("and a manifest csv is loaded", function () {
+          beforeEach(function(){
             runs(function () {
-              // TODO: the expected data will need updating here
-              expectedData = [
-                [
-                  [
-                    {
-                      type:     'GET',
-                      url:      '/',
-                      dataType: 'json',
-                      data:     null
-                    }
-                  ],
-                  [
-                    {
-                      type:     'POST',
-                      url:      '/actions/bulk_update_sample',
-                      dataType: 'json',
-                      headers:  { "Content-Type": 'application/json' },
-                      data:     '{"bulk_update_sample":{"user":"username","by":"sanger_sample_id","updates":{"S2-13a8da96d0e34db1ac7d7c40159a2095":{"volume":2,"cellular_material":{"lysed":true}},"S2-cb4ee4768f334c38960ac89ec2074eb1":{"volume":2,"cellular_material":{"lysed":true}}}}}'
-                    }
-                  ]
-                ]
-              ];
+              results.resetFinishedFlag();
+              fakeContent()
+                .find("#read-manifest-btn")
+                .trigger('click');
 
-              spyOn(config, "ajax").andCallThrough();
-              presenter.responderCallback(manifestCSVData)
+              //wait for jquery animation
+              waits(500);
+
+              //simulate file input
+              presenter.manifestReaderComponent.presenter.responderCallback(manifestCSVData)
                 .then(function () {
                   FakeUser.waitsForIt(fakeDOM, "#registrationBtn", results.expected);
                 })
                 .fail(results.unexpected);
             });
-
             waitsFor(results.hasFinished);
+          });
 
+          describe("and a valid barcode is entered", function(){
+            beforeEach(function(){
+              runs(function () {
+                results.resetFinishedFlag();
+                fakeContent()
+                  .find('.barcodeInput')
+                  .val("2881460250710")
+                  .trigger(FakeUser.aPressReturnEvent());
+
+                FakeUser.waitsForIt(fakeContent(),
+                  "tbody tr.selectedRow",
+                  results.expected
+                );
+              });
+              waitsFor(results.hasFinished);
+            });
+
+            it("highlights the row", function () {
+              runs(function () {
+                expect(fakeContent().find('tbody tr.selectedRow').length).toEqual(1);
+              });
+            });
+
+            it("removes the barcode from the input", function () {
+              runs(function () {
+                expect(fakeContent().find('.barcodeInput').val()).toEqual('')
+              });
+            });
+
+            it("the checkbox is enabled on the highlighed row", function(){
+              runs(function(){
+                expect(fakeContent().find('tbody tr.selectedRow td:first input').attr("checked")).toEqual("checked");
+              });
+            });
+
+            it("the other checkbox is disabled", function(){
+              runs(function(){
+                expect(fakeContent().find('tbody tr.disabledRow td:first input').attr("disabled")).toEqual("disabled");
+              });
+            });
+
+            it("after the checkbox is un-checked, the row it belongs to is disabled", function(){
+              runs(function(){
+                results.resetFinishedFlag();
+                fakeContent()
+                  .find('tbody tr.selectedRow td:first input')
+                  .trigger('click');
+
+//                FakeUser.waitsForIt(
+//                  "tbody tr:first .disabledRow",
+//                  results.expected
+//                );
+              });
+//              waitsFor(results.hasFinished);
+
+              waits(500);
+
+              runs(function(){
+                expect(fakeContent().find('tbody tr.disabledRow').length).toEqual(2);
+              });
+            });
+          });
+
+          it("makes the correct call when 'register' is clicked", function () {
+            var expectedData;
             runs(function () {
+              expectedData = {
+                type:     'POST',
+                url:      '/actions/bulk_update_sample',
+                dataType: 'json',
+                headers:  { 'Content-Type': 'application/json' },
+                data:     '{"bulk_update_sample":{"user":"username","by":"sanger_sample_id","updates":{"S2-13a8da96d0e34db1ac7d7c40159a2095":{"volume":2,"cellular_material":{"lysed":true}},"S2-cb4ee4768f334c38960ac89ec2074eb1":{"volume":2,"cellular_material":{"lysed":true}}}}}'
+              };
+
+              spyOn(config, "ajax").andCallThrough();
+
               results.resetFinishedFlag();
               fakeContent()
                 .find('#registrationBtn')
@@ -103,7 +149,7 @@ define([
             waits(500);
 
             runs(function () {
-              expect(config.ajax).toHaveBeenCalledWith(_.flatten(expectedData));
+              expect(config.ajax).toHaveBeenCalledWith(expectedData);
             });
           });
         });
@@ -133,9 +179,18 @@ define([
         });
 
         describe("and there is input request for 3 tubes", function () {
-          it("makes an appropriate ajax call", function(){
+          it("makes an appropriate ajax call", function () {
+            var expectedData;
             runs(function () {
               spyOn(config, 'ajax').andCallThrough();
+              expectedData = {
+                type:     'POST',
+                url:      '/actions/bulk_create_sample',
+                dataType: 'json',
+                headers:  { 'Content-Type': 'application/json' },
+                data:     '{"bulk_create_sample":{"user":"username","state":"draft","quantity":3,"sample_type":"RNA","sanger_sample_id_core":"QC1Hip"}}'
+              };
+
               fakeContent()
                 .find('#number-of-sample')
                 .val(3)
@@ -149,9 +204,8 @@ define([
             });
             waits(500);
 
-            runs(function(){
-              expect(config.ajax).toHaveBeenCalled();
-              expect(config.ajax).toHaveBeenCalledWith('derp');
+            runs(function () {
+              expect(config.ajax).toHaveBeenCalledWith(expectedData);
             });
           });
         });
