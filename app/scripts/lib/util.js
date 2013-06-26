f1ca0e18f42a9589407d169b714a160520e9704d
David Allen <da6@sanger.ac.uk>
2013-06-26 12:18:14 +0100

    deep merge function added to util library. Tests added that demonstrate that this function operates as expected

--- app/scripts/lib/util.js
+++ app/scripts/lib/util.js
@@ -11,6 +11,30 @@ define([], function () {
       pad = pad || '0';
       validLength = validLength || 13;
       return barcode.length >= validLength ? barcode : new Array(validLength - barcode.length + 1).join(pad) + barcode;
+    },
+
+    // Merges two objects recursively
+    deepMerge: function (foo, bar) {
+      var util = this;
+      var merged = {};
+      for (var each in bar) {
+        if (foo.hasOwnProperty(each) && bar.hasOwnProperty(each)) {
+          if (typeof(foo[each]) === "object" && typeof(bar[each]) === "object") {
+            merged[each] = util.deepMerge(foo[each], bar[each]);
+          } else {
+            // chooses bar's version if both objects possess the key
+            merged[each] = bar[each];
+          }
+        } else if (bar.hasOwnProperty(each)) {
+          merged[each] = bar[each];
+        }
+      }
+      for (var each in foo) {
+        if (!(each in bar) && foo.hasOwnProperty(each)) {
+          merged[each] = foo[each];
+        }
+      }
+      return merged;
     }
   });
 
