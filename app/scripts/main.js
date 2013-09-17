d57df76367a862f0a014f5543ad7f6c262d1dc9f
Sean Dunn <sd9@sanger.ac.uk>
2013-09-17 18:57:24 +0100

    Remove unused references from main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,30 +2,28 @@ require.config({
   shim:{
   },
 
-  baseUrl: 'scripts',
+  baseUrl: "scripts",
 
   paths:{
-    hm:                   'vendor/hm',
-    esprima:              'vendor/esprima',
-    jquery:               'vendor/jquery.min',
-    text:                 '../components/requirejs-text/text',
-    spinjs:               '../components/spin.js/spin',
-    labware:              'labware',
-    mapper:               '../components/S2Mapper/app/scripts/mapper',
-    mapper_services:      '../components/S2Mapper/app/scripts/services',
-    mapper_test:          '../components/S2Mapper/test',
-    mapper_testjson:      '../components/S2Mapper/test/json',
-    config:               'config',
-    reception_templates:  'lib/reception_templates',
-    images:               '../images'
+    bootstrap:            "vendor/bootstrap/bootstrap",
+    text:                 "../components/requirejs-text/text",
+    spinjs:               "../components/spin.js/spin",
+    labware:              "labware",
+    mapper:               "../components/S2Mapper/app/scripts/mapper",
+    mapper_services:      "../components/S2Mapper/app/scripts/services",
+    mapper_test:          "../components/S2Mapper/test",
+    mapper_testjson:      "../components/S2Mapper/test/json",
+    config:               "config",
+    reception_templates:  "lib/reception_templates",
+    images:               "../images"
   }
 });
 
-require(['app',
-  'controllers/controller_factory'
+require(["app",
+  "controllers/controller_factory"
 ], function (App, ControllerFactory) {
+  "use strict";
 
-    var theApp = new App(new ControllerFactory());
-
+  var theApp = new App(new ControllerFactory());
 });
 
