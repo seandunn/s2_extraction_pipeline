e3c85053a436f656bf53b8802179ded4753c48b4
Matthew Denner <md12@sanger.ac.uk>
2013-09-19 13:26:05 +0100

    Parallise manifest generation
    Samples & barcodes can be created in parallel.
    Pair up samples with barcodes.
    Put samples in tubes & label tubes with associated barcode.
    Generate a manifest based on sample+barcode pairs.
    The last two can be run in parallel.

--- app/scripts/models/manifest_maker_model.js
+++ app/scripts/models/manifest_maker_model.js
@@ -21,9 +21,7 @@ define([
     reset:function(){
       delete this.currentTemplateBlob;
       delete this.manifestCsvBlob;
-      delete this.samples;
       delete this.labwareOutputs;
-      delete this.barcodes;
     },
 
     generateSamples: function (templateName, study, sampleType, nbOfSample) {
@@ -36,23 +34,6 @@ define([
           .then(function(templateBlob){
             thisModel.currentTemplateBlob = templateBlob;
             return thisModel.getLabellables(templateName, study, sampleType, nbOfSample);
-          })
-          .fail(function(error){
-            return deferred.reject({message: " Couldn't produce the samples. " + error.message, previous_error: error});
-          })
-          .then(function () {
-            return thisModel.generateCSVBlob(sampleType);
-          })
-          .fail(function (error) {
-            return deferred.reject({message: " Couldn't produce barcode data file. " + error.message, previous_error: error});
-          })
-          .then(function (manifestCsvBlob) {
-            thisModel.manifestCsvBlob = manifestCsvBlob;
-            // now we are ready...
-            return thisModel.sendManifestRequest(thisModel.currentTemplateBlob, thisModel.manifestCsvBlob);
-          })
-          .fail(function(error){
-            return deferred.reject({message:" >> "+error.message, previous_error:error});
           })
           .then(function(){
             return deferred.resolve();
@@ -78,165 +59,210 @@ define([
 
     getLabellables: function (templateName, studyName, sampleType, nbOfSample) {
       var deferred = $.Deferred();
+      var fail     = function(m) { return deferred.fail({message: m}); };
+
       var thisModel = this;
       var root;
       var labwareModel = ReceptionTemplates[templateName].model;
       var sangerSampleIdCore = ReceptionStudies[studyName].sanger_sample_id_core;
+      var aliquotType = ReceptionTemplates[templateName].aliquot_type;
+
       thisModel.owner.getS2Root()
-          .fail(function () {
-            return deferred.reject({message: "Couldn't get the root! Is the server accessible?"});
-          })
-          .then(function (result) {
-            root = result;
-            // creation of the samples
-            return root.bulk_create_samples.create({
-              state:     "draft",
-              quantity:  nbOfSample,
-              sample_type: sampleType,
-              sanger_sample_id_core:sangerSampleIdCore
-            });
-          })
-          .fail(function () {
-            return deferred.reject({message: "Couldn't create the empty samples on S2."});
-          })
-          .then(function (bulkCreationSampleObject) {
-            // might be useful to instantiate the samples at some point....
-            // For now, we just use them as bare objects
-            thisModel.samples = bulkCreationSampleObject.result.samples;
-            var tubes = _.map(thisModel.samples, function (sample) {
-              return {
-                aliquots:[{
-                  "sample_uuid": sample.uuid,
-                  "type": ReceptionTemplates[templateName].aliquot_type
-                }
-                ]
-              };
-            });
-            // creation of the piece of labware
-            return root["bulk_create_" + labwareModel.pluralize()].create({"tubes": tubes});
-          })
-          .fail(function () {
-            return deferred.reject({message: "Couldn't register the associated piece of labware."});
-          })
-          .then(function (bulkCreationLabwareObject) {
-            thisModel.labwareOutputs = _.map(bulkCreationLabwareObject.result.tubes, function (rawTube) {
-              return root.tubes.instantiate({rawJson:{tube:rawTube}});
-            });
-            // creation of the barcodes
-            return root.bulk_create_barcodes.create({
-              "number_of_barcodes": nbOfSample,
-              "labware":  labwareModel,
-              "role":     "stock",
-              "contents": sampleType
-            });
-          })
-          .fail(function () {
-            return deferred.reject({message: "Couldn't create the barcodes."});
-          })
-          .then(function (bulkCreationBarcodeObject) {
-            thisModel.barcodes = bulkCreationBarcodeObject.result.barcodes;
-            var labels = _.map(thisModel.barcodes, function (label) {
-              return {
-                barcode:        {
-                  type:  "ean13-barcode",
-                  value: label.ean13
-                },
-                "sanger label": {
-                  type:  "sanger-barcode",
-                  value: label.sanger.prefix + label.sanger.number + label.sanger.suffix
-                }
-              };
-            });
-            var bulkData = _.map(thisModel.labwareOutputs, function (labware) {
-              // NOTE: in theory, one can associate a labware to any barcode.
-              // However, in the test data, because the tests nmight a bit too strict
-              // this order can become relevant... Be careful when running tests.
-              return {"name": labware.uuid, "type": "resource", labels:labels.pop()};
-            });
-            // link barcodes to labware
-            return root.bulk_create_labellables.create({
-              labellables:   bulkData
-            });
-          })
-          .fail(function () {
-            return deferred.reject({message: "Couldn't associate the barcodes to the tubes."});
-          })
-          .then(function (bulkCreationLabellableObject) {
-            _.each(thisModel.labwareOutputs, function (output) {
-              var matchingLabellable = _.chain(bulkCreationLabellableObject.result.labellables).find(function (labellable) {
-                return labellable.name === output.uuid;
-              }).value();
-              output.labels = matchingLabellable.labels;
-            });
-            deferred.resolve(thisModel.labwareOutputs);
-          });
+          .then(function(root) {
+            // We know, up front, how many samples are being created and therefore how many barcodes
+            // we're going to need at the end of the process.  Hence, we can perform the bulk sample
+            // and bulk barcode creation in parallel.
+            var samples  = preRegisterSamples(root, nbOfSample, sampleType, sangerSampleIdCore).fail(fail);
+            var barcodes = preRegisterBarcodes(root, nbOfSample, labwareModel, sampleType).fail(fail);
+
+            return $.when(samples, barcodes).then(function(samples, barcodes) {
+              // We can pair up the samples to a barcode now
+              thisModel.sampleBarcodes = 
+                _.chain(samples)
+                 .zip(barcodes)
+                 .indexBy(function(pair) { return pair[0].uuid; })
+                 .value();
+              return thisModel;
+            }).then(function(model) {
+              // We can create the labware and label it at the same time as producing the
+              // manifest XLS file
+              var manifest = generateManifest(sampleType, thisModel.currentTemplateBlob, thisModel.sampleBarcodes).fail(fail);
+              var tubes    = createLabelledTubes(thisModel, root, labwareModel, aliquotType).fail(fail);
+              return $.when(tubes, manifest);
+            })
+            .then(function(labellables, manifest) {
+              thisModel.manifestBlob = manifest;
+              deferred.resolve(thisModel);
+            }, _.partial(fail, "Couldn't associate the barcodes to the tubes."))
+          }, _.partial(fail, "Couldn't get the root! Is the server accessible?"));
 
       return deferred.promise();
-    },
-
-    generateCSVBlob: function(sampleType){
-      var thisModel = this;
-      var data = _.map(thisModel.labwareOutputs,function(labware){
-        var sampleUUID = labware.aliquots[0].sample.uuid;
-        var sanger_sample_id = _.find(thisModel.samples,function (sample) {
-          return sample.uuid === sampleUUID;
-        }).sanger_sample_id;
-        return [labware.labels.barcode.value, 
-                labware.labels['sanger label'].value, 
-                sanger_sample_id, 
-                sampleType]
-                .join(',');
-      });
-      data.unshift("Tube Barcode,Sanger Barcode,Sanger Sample ID,SAMPLE TYPE");
-      var txt = data.join("\n");
-      return new Blob([txt], { "type" : "text\/csv" })
     },
 
     printBarcodes: function (printerName) {
       var deferred = $.Deferred();
       var thisModel = this;
       BasePageModel.printBarcodes(thisModel.labwareOutputs, printerName)
-          .fail(function(){
-            return deferred.reject({message: "Couldn't print the barcodes."});
-          })
-          .then(function(){
-            return deferred.resolve(thisModel);
-          });
+                   .then(function(){
+                     return deferred.resolve(thisModel);
+                   }, function(){
+                     return deferred.reject({message: "Couldn't print the barcodes."});
+                   })
       return deferred.promise();
     },
 
-    sendManifestRequest: function (templateBlob,manifestBlob) {
-      var deferred = $.Deferred();
-      var thisModel = this;
-      try {
-        var xhr = new XMLHttpRequest;
-        xhr.open("POST", 'http://psd2g.internal.sanger.ac.uk:8100/manifest-merge-service/');
-        xhr.responseType = "blob";
-        xhr.onerror = function (oEvent) {
-          deferred.reject({
-            message: "Unable to send the manifest... Is the XLS merger server up and running? "
-          });
-        };
-        xhr.onload = function (oEvent) {
-          thisModel.manifestBlob = this.response;
-          deferred.resolve(thisModel);
-        };
-
-        var form = new FormData();
-        form.append("template",templateBlob);
-        form.append("manifest-details",manifestBlob);
-
-        xhr.send(form);
-      } catch (err) {
-        var message = " msg:" + err.message;
-        message += "\n" + err.code;
-        message += "\n" + err.name;
-        message += "\n" + err.stack;
-        deferred.reject({message: message});
-      }
-      return deferred.promise();
-    }
   });
 
   return ReceptionModel;
+
+  // These two functions can run in parallel
+  function preRegisterSamples(root, number, type, core) {
+    return root.bulk_create_samples.create({
+      state:                 "draft",
+      quantity:              number,
+      sample_type:           type,
+      sanger_sample_id_core: core
+    }).then(function(action) {
+      return action.result.samples;
+    }, function() {
+      return "Could not pre-register " + nbOfSample + " samples in S2.";
+    });
+  }
+  function preRegisterBarcodes(root, number, model, type) {
+    return root.bulk_create_barcodes.create({
+      number_of_barcodes: number,
+      labware:            model,
+      role:               "stock",
+      contents:           type
+    }).then(function(action) {
+      return action.result.barcodes;
+    }, function() {
+      return "Could not pre-register " + number + " barcodes in S2.";
+    });
+  }
+
+  // These two functions can be run in parallel
+  function generateManifest(type, template, sampleBarcodes) {
+    return sendManifestRequest(
+      template,
+      generateCSVBlob(sampleBarcodes, type)
+    );
+  }
+  function createLabelledTubes(model, root, labwareModel, aliquotType) {
+    // Creates tubes in the system that hold each of the samples, and then labels those tubes
+    // with the barcode that was attached to the sample the tube contains.
+    var builder = function(raw) {
+      return root[labwareModel.pluralize()].instantiate(_.build('rawJson', labwareModel, raw));
+    };
+
+    var tubes =
+      _.chain(model.sampleBarcodes)
+       .values()
+       .map(_.first)
+       .map(_.partial(createAliquotForSample, aliquotType))
+       .value();
+
+    return root["bulk_create_" + labwareModel.pluralize()].create({
+      tubes: tubes
+    }).then(function (bulkCreationLabwareObject) {
+      model.labwareOutputs =
+        _.chain(bulkCreationLabwareObject.result[labwareModel.pluralize()])
+         .map(builder)
+         .map(_.partial(bindBarcodeToTube, model.sampleBarcodes))
+         .value();
+
+      return root.bulk_create_labellables.create({
+        labellables: _.map(model.labwareOutputs, labellableForResource)
+      }).fail(function() {
+        return "Couldn't connect the tubes to their labels in S2.";
+      });
+    }, function() {
+      return "Couldn't register the associated piece of labware."
+    });
+  }
+
+  function bindBarcodeToTube(sampleBarcodes, tube) {
+    tube.labels = sampleBarcodes[tube.aliquots[0].sample.uuid][1];
+    return tube;
+  }
+
+  function labelForBarcode(barcode) {
+    return {
+      barcode:        {
+        type:  "ean13-barcode",
+        value: barcode.ean13
+      },
+      "sanger label": {
+        type:  "sanger-barcode",
+        value: barcode.sanger.prefix + barcode.sanger.number + barcode.sanger.suffix
+      }
+    };
+  }
+
+  function labellableForResource(resource) {
+    return {
+      name: resource.uuid,
+      type: "resource",
+      labels: labelForBarcode(resource.labels)
+    };
+  }
+
+  function createAliquotForSample(type, sample) {
+    return {
+      aliquots:[{
+        "sample_uuid": sample.uuid,
+        "type": type
+      }]
+    };
+  }
+
+  function generateCSVBlob(sampleBarcodes, type){
+    var csv =
+      _.chain(sampleBarcodes)
+       .values()
+       .map(_.partial(csvRow, type))
+       .unshift([["Tube Barcode", "Sanger Barcode", "Sanger Sample ID", "SAMPLE TYPE"]])
+       .map(function(row) { return row.join(","); })
+       .value()
+       .join("\n");
+
+    return new Blob([csv], { "type" : "text\/csv" })
+  }
+  function csvRow(type, pair) {
+   var sample = pair[0], label = pair[1];
+   return [
+     label.ean13,
+     label.sanger.prefix + label.sanger.number + label.sanger.suffix,
+     sample.sanger_sample_id,
+     type
+   ];
+  }
+
+  // TODO: Use jQuery because this is fugly
+  function sendManifestRequest(templateBlob,manifestBlob) {
+    var deferred = $.Deferred();
+    try {
+      var xhr = new XMLHttpRequest;
+      xhr.open("POST", 'http://psd2g.internal.sanger.ac.uk:8100/manifest-merge-service/');
+      xhr.responseType = "blob";
+      xhr.onerror = function (oEvent) {
+        deferred.reject("Unable to send the manifest... Is the XLS merger server up and running?");
+      };
+      xhr.onload = function (oEvent) {
+        deferred.resolve(this.response);
+      };
+
+      var form = new FormData();
+      form.append("template",templateBlob);
+      form.append("manifest-details",manifestBlob);
+      xhr.send(form);
+    } catch (err) {
+      var message = " msg:" + err.message;
+      message += "\n" + err.code;
+      message += "\n" + err.name;
+      message += "\n" + err.stack;
+      deferred.reject(message);
+    }
+    return deferred.promise();
+  }
 });
