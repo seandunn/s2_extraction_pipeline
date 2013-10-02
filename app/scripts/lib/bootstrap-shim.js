7254117ab7195fada0eddc98fa6bc9f73ec88ef3
Sean Dunn <sd9@sanger.ac.uk>
2013-10-02 17:58:56 +0100

    Add all the bootstrap.js files to the shim

--- app/scripts/lib/bootstrap-shim.js
+++ app/scripts/lib/bootstrap-shim.js
@@ -1,6 +1,21 @@
 // Bootstrap needs jQuery loaded before it does anything!
 define(function(require) {
+  "use strict";
+
   require("jquery");
+  // require("bootstrap/bootstrap-affix");
+  // require("bootstrap/bootstrap-alert");
+  // require("bootstrap/bootstrap-button");
+  // require("bootstrap/bootstrap-carousel");
   require("bootstrap/bootstrap-collapse");
+  // require("bootstrap/bootstrap-dropdown");
+  // require("bootstrap/bootstrap-modal");
+  // require("bootstrap/bootstrap-popover");
+  // require("bootstrap/bootstrap-scrollspy");
+  require("bootstrap/bootstrap-tab");
+  // require("bootstrap/bootstrap-tooltip");
+  require("bootstrap/bootstrap-transition");
+  // require("bootstrap/bootstrap-typeahead");
+
   return {};
 });
