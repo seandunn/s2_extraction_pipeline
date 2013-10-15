17e2ae876e1fe33dc89cb33666a261474fb929a2
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2013-10-15 17:15:59 +0100

    Testing code for using tecan layout generator
    Adds tecan file generation from list of labware sources and

--- app/scripts/lib/file_handling/tecan.js
+++ app/scripts/lib/file_handling/tecan.js
@@ -1,67 +1,157 @@
 define([ 'lib/underscore_extensions' ], function() {
   'use strict';
                       
-
-  return {
-      parse: function(obj, total_volume) {
-          return header(obj) + dynMappings(obj) + "C;\n" + buffers(obj, total_volume) + footer(obj);
-      },
-      to : function(userName, date, requests) {
-          return objToTecanFile(_.chain(requests).filter(function(request) {
-              return request.state === 'passed';
-          }).reduce(function(obj, item) {
-              var plateSrc = item.asset.plate;
-              var plateDst = item.target_asset.plate;
-              obj.source[plateSrc.ean13_barcode] = {
-                      "name" : plateSrc.stock_plate_name.replace(/_/, " "),
-                      "plate_size" : plateSrc.size };
-              obj.destination[plateDst.ean13_barcode] = {
-                      "name" : getCherryPickableDefaultName(),
-                      "plate_size" : plateDst.size };
-              if (_.isUndefined(obj.destination[plateDst.ean13_barcode].mapping)) {
-                  obj.destination[plateDst.ean13_barcode].mapping = [];
-              }
-              obj.destination[plateDst.ean13_barcode].mapping.push({
-                  "src_well" : [
-                                plateSrc.ean13_barcode,
-                                item.asset.map.description ],
-                                "dst_well" : item.target_asset.map.description,
-                                "volume" : (item.target_asset.get_picked_volume) });
-              return obj;
+  var tecanImpl = {
+          /**
+           *  Main action of tecan generation file. Executes a layout process and returns a string with tecan file contents 
+           *  @arguments:
+           *  sources: Array of labware elements that will be source in transfer
+           *  destination: Single labware that acts as destination in transfer
+           *  username: name of user to be printed in file generated
+           *  date: timestamp for printing in file generated
+           *  @return: a string with the contents of the tecan file
+           **/
+          to: function(sources, destination, /* optional */ username, /* optional */ date) {
+              tecanImpl.setConfig(username, date, layoutColumnMajorOrder, workingDilution, tecanFile);
+              return tecanImpl.to.apply(this, arguments);
           },
-          { user : userName, time : date,
-              source : {}, destination : {} }));
-      } 
+          
+          /**
+           * Modifies behaviour of main tecan method (to) by changing its stages implementation.
+           * @arguments:
+           * username: name of user to be printed in file generated
+           * date: timestamp for printing in file generated
+           * layoutStrategy: assigns wells from sources to wells in destination
+           * outputStrategy: defines amount of buffer and volume that will be transferred for each well
+           * fileGeneration: parses config object and returns a string with the contents of the tecan file
+           * @return: a string with the contents of the tecan file
+           */
+          setConfig: function(username, date, layoutStrategy, outputStrategy, fileGeneration) {
+              tecanImpl.to = _.partial(function(layoutStrategy, outputStrategy, fileGeneration, sources, destination) {
+                  return _.compose(
+                          fileGeneration, 
+                          _.partial(outputStrategy, sources, destination), 
+                          _.partial(layoutStrategy, sources, destination), 
+                          _.partial(buildEmptyLayout, username, date, sources, destination))();
+              }, layoutStrategy, outputStrategy, fileGeneration);
+              return tecanImpl.to;
+          },
+          /**
+           * Parses a config object and returns a string with the contents of the tecan file
+           * @arguments:
+           * data: JS object with the configuration of the file to generate
+           * @return: a string with the contents of the tecan file
+           */
+          parse: tecanFile
   };
+  
+  return tecanImpl;
+ 
+  /* Helper functions */
+  function getPlateSize(labware) {
+      return labware.number_of_rows*labware.number_of_columns;
+  }  
+  
+  /* Stages: */
+  /* Stage1: Layout constructor */
+  function buildEmptyLayout(username, date, sourceList, destination) {
+      var initialValue = { source: {}, target: {} };
+      initialValue.user = (username ? username: 'defaultUser');
+      initialValue.time = (date ? date: (new Date()).toString());
+      initialValue.target[destination.labels.barcode.value] = { 
+              mapping: [], 
+              plateSize:  getPlateSize(destination) 
+      };
+      return initialValue;
+  }  
+  
+  /* Stage2: Layout strategies */
+  function layoutColumnMajorOrder(sourceList, destination, layout) {      
+      function nextPositionIterator(destination) {
+          var keysList=[];
+          for (var i=0; i<destination.number_of_rows; i++) {
+              for (var j=0; j<destination.number_of_columns; j++) {
+                  keysList.push([String.fromCharCode('A'.charCodeAt(0)+i),
+                                 String.fromCharCode('1'.charCodeAt(0)+j)].join(''));
+              }
+          }
+          return _.partial(function(keys) {
+            return keys.shift();
+          }, _.filter(keysList, function(val) {
+              return !(val in _.keys(destination.tubes));
+          }));
+      }
       
-  function getCherryPickableDefaultName() {
-      //PlatePurpose.cherrypickable_default_type.first.name.replace(/_/, "\s");
-      return 'default name';
+      var nextPosition = nextPositionIterator(destination);
+      var destinationMapping;
+      return _.reduce(sourceList, function(layout, source) {
+          return _.pairs(source.tubes).reduce(function(layout, tube) {
+              if (_.isUndefined(destinationMapping)) {
+                  destinationMapping = layout.target[destination.labels.barcode.value].mapping;                  
+              }
+              destinationMapping.push({
+                  srcWell: [ source.labels.barcode.value, tube[0] ],
+                  targetWell: nextPosition()
+              });
+              layout.source[source.labels.barcode.value] = {
+                      plateSize: getPlateSize(source)
+              };
+              return layout;
+          }, layout);
+      }, layout);
   }
-                
+
+  /* Stage3: Output strategies */
+  function workingDilution(sourceList, destination, layout) {
+      _.each(layout.target, function(target) {
+          _.each(target.mapping, function(mapping) {
+              mapping.buffer=2.0;
+              mapping.amount=23.0;
+          });
+      });
+      return layout;
+  }
+  
+  /* Stage4: Tecan file generation */
+  function tecanFile(layout) {
+      return _.compose(
+              _.partial(footer, layout),  
+              _.partial(buffers, layout), 
+              _.partial(separator, layout), 
+              _.partial(dynMappings, layout), 
+              _.partial(header, layout))(layout);
+  }
+  
+  function separator(obj, text) {
+      return text + "C;\n";      
+  }
+  
+  /* File Generation functions */                  
   function header(data) {
-      return "C;\nC; This file created by "+ data.user + " on " + data.time + "\nC;\n";
+      return "C;\nC; This file created by " + data.user + " on " + data.time + "\nC;\n";
   }
                 
-  function dynMappings(obj){
+  function dynMappings(obj, text){
       var mappingByWell = [];
       var dyn_mappings = '';
-      _.chain(obj.destination).each(function(destData, destBarcode) {
+      _.chain(obj.target).each(function(destData, destBarcode) {
           _.each(destData.mapping, function(mapping) {
-              mappingByWell[descriptionToVerticalPlatePosition(mapping.dst_well, destData.plate_size)] = mapping;
+              mappingByWell[descriptionToVerticalPlatePosition(mapping.targetWell, destData.plateSize)] = mapping;
           });
 
           _.chain(mappingByWell).each(function(mapping, dest_position) {
-              var source_barcode = mapping.src_well[0];
+              var source_barcode = mapping.srcWell[0];
               var source_name = obj.source[source_barcode].name;
-              var source_position  = descriptionToVerticalPlatePosition(mapping.src_well[1], obj.source[mapping.src_well[0]].plate_size);
-              var destination_position = descriptionToVerticalPlatePosition(mapping.dst_well, destData.plate_size);
-              dyn_mappings += ["A", source_barcode, "", source_name, source_position, "", mapping.volume + "\nD", 
-                               destBarcode, "", destData.name, destination_position, "", mapping.volume + "\nW", "\n"].join(";");
+              var source_position  = descriptionToVerticalPlatePosition(mapping.srcWell[1], 
+                      obj.source[mapping.srcWell[0]].plateSize);
+              var destination_position = descriptionToVerticalPlatePosition(mapping.targetWell, destData.plateSize);
+              dyn_mappings += ["A", source_barcode, "", source_name, source_position, "", 
+                               mapping.amount + "\nD", destBarcode, "", destData.name, destination_position, "", 
+                               mapping.amount + "\nW", "\n"].join(";");
 
           });
       });
-      return dyn_mappings;
+      return text + dyn_mappings;
   }
 
   function descriptionToVerticalPlatePosition(wellDescription, plateSize) {
@@ -77,34 +167,32 @@ define([ 'lib/underscore_extensions' ], function() {
       }).value()[0];
   }
                 
-  function buffers(obj, total_volume) {
+  function buffers(obj, text) {
       var buffer = [];
       var mappingByWell = [];
-      _.chain(obj.destination).each(function(destData, destBarcode){
+      _.chain(obj.target).each(function(destData, destBarcode){
           mappingByWell=[];
           _.each(destData.mapping, function(mapping) {
-              mappingByWell[descriptionToVerticalPlatePosition(mapping.dst_well, destData.plate_size)] = mapping;
+              mappingByWell[descriptionToVerticalPlatePosition(mapping.targetWell, destData.plateSize)] = mapping;
           });
           _.chain(mappingByWell).each(function(mapping, dest_position) {
-              if (total_volume > mapping.volume)
-              {
-                  var dest_name = obj.destination[destBarcode].name;
-                  var volume = ((total_volume*100) - (mapping.volume * 100))/100;
-                  var vert_map_id = descriptionToVerticalPlatePosition(mapping.dst_well, destData.plate_size);
-                  buffer.push(["A", "BUFF", "", "96-TROUGH", vert_map_id, "", volume+"\nD", destBarcode, "", dest_name, 
-                               vert_map_id, "", volume+"\nW", ""].join(';'));
-              }
+              var destName = obj.target[destBarcode].name;
+              var bufferAmount = mapping.buffer;
+              var vert_map_id = descriptionToVerticalPlatePosition(mapping.targetWell, destData.plateSize);
+              buffer.push(["A", "BUFF", "", "96-TROUGH", vert_map_id, "", 
+                           bufferAmount+"\nD", destBarcode, "", destName, 
+                           vert_map_id, "", bufferAmount+"\nW", ""].join(';'));
           });
       });
-      return buffer.join("\n");
+      return text + buffer.join("\n");
   }
 
-  function footer(obj) {
+  function footer(obj, text) {
       var footerText = "\nC;\n";
 
-      _.chain(obj.destination).values().reduce(function (value, dest) { return value.concat(dest.mapping); }, [])
-      .filter(function(mapWell) { return !_.isUndefined(mapWell.src_well) && !_.isUndefined(mapWell.src_well[0]);})
-      .map(function(mapWell) { return mapWell.src_well[0];})
+      _.chain(obj.target).values().reduce(function (value, dest) { return value.concat(dest.mapping); }, [])
+      .filter(function(mapWell) { return !_.isUndefined(mapWell.srcWell) && !_.isUndefined(mapWell.srcWell[0]);})
+      .map(function(mapWell) { return mapWell.srcWell[0];})
       .uniq().reduce(function(barcodeLookup, plate, index) {
           barcodeLookup[plate] = index + 1;
           return barcodeLookup;
@@ -115,14 +203,13 @@ define([ 'lib/underscore_extensions' ], function() {
 
       footerText += "C;\n";
 
-      _.chain(obj.destination).keys().reduce(function(barcodeLookup, keyPlate, index) {
+      _.chain(obj.target).keys().reduce(function(barcodeLookup, keyPlate, index) {
           barcodeLookup[keyPlate] = index + 1;
           return barcodeLookup;
       }, {}).pairs().sort(function(a,b) { return a[1] > b [1]; })            
       .each(function(barcodeInfo) {
           footerText += ["C; DEST", barcodeInfo[1], " = ", barcodeInfo[0], "\n"].join('');
       });
-
-      return footerText;
-  }     
+      return text + footerText;
+  }
 });
--- test/spec/file_handling/tecan_spec.js
+++ test/spec/file_handling/tecan_spec.js
@@ -1,34 +1,45 @@
-define(['lib/file_handling/tecan'], function(tecanHandler) {
+define(['lib/file_handling/tecan', 'mapper/s2_root'], function(tecanHandler, root) {
   'use strict';
 
   return function() {      
     describe("Tecan Files", function(){
       var tecanData;
       var tecanFile;
+      var layoutTestFile;
             
       beforeEach(function(done){
           var files= {
           };
-          $.when.apply(this, _.map(['../test/data/gwl/127073.gwl', '../test/data/gwl/127073.json'], function(path) {
+          var sourceList = [];
+          $.when.apply(this, _.map(['../test/data/gwl/127073.gwl', '../test/data/gwl/127073.json', '../test/data/gwl/layout_test.gwl'], function(path) {
               return $.ajax(path, {success: _.partial(function(url, data) {
                   files[url]=data;
               }, path)}).promise();
           })).then(function() {
               tecanFile = files['../test/data/gwl/127073.gwl'];
               tecanData = files['../test/data/gwl/127073.json'];
+              layoutTestFile = files['../test/data/gwl/layout_test.gwl'];
               done();
-          }).fail(function() {
-              console.log('error executing');
           });
       });
 
-      it("can parse a Tecan File", function(){
-        var tecanParsing = tecanHandler.parse(tecanData, 13);
+      it("can create a Tecan File from a predefined layout", function(){
+        var tecanParsing = tecanHandler.parse(tecanData);
         var result = tecanParsing.match(tecanFile);
         expect(result !==null).to.equal(true);
       });
-
-    });
-
-  };
+      
+      it("can do a new layout from a list of sources and a destination", function(done) {
+          var destination = {number_of_rows:12, number_of_columns: 8, labels: { barcode: { value: '12345'}}, tubes: {}};
+          var userName = "TEST_USER_1";
+          root.load({user:userName}).done(function(results) { 
+              results.findByLabEan13('3960299138815').done(function(source) {
+                  var parsing = tecanHandler.to([source], destination, userName, 'Wed Oct 16 2013 16:33:33 GMT+0100 (BST)');
+                  expect(parsing).to.equal(layoutTestFile);
+                  done();                  
+              });
+          });
+      });
+  });
+};
 });
