1c57d2c06539d71664ce3a1babbf043d68eff97a
Sean Dunn <sd9@sanger.ac.uk>
2013-10-30 12:25:30 +0000

    Remove lysing from lab-activities

--- app/scripts/app-components/lab-activities/lab-activities.js
+++ app/scripts/app-components/lab-activities/lab-activities.js
@@ -1,15 +1,13 @@
 define([
   "app-components/process-choice/process-choice",
-  "app-components/re-racking/re-racking",
-  "app-components/lysing/lysing"
-], function(ProcessChoice, Reracking, Lysing) {
+  "app-components/re-racking/re-racking"
+], function(ProcessChoice, Reracking) {
   "use strict";
 
   return function(context) {
     return new ProcessChoice(_.extend({
       components: [
-        {label: "Re-racking", id: "re-racking", constructor: Reracking},
-        {label: "Lysing",     id: "lysing",     constructor: Lysing}
+        {label: "Re-racking", id: "re-racking", constructor: Reracking}
       ]
     }, context));
   };
