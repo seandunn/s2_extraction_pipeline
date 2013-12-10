29b1785ecc233a35547e741fc723fb9c13e4e10e
Eduardo Martin Rojo <emr@mib105656i.local>
2013-12-10 16:54:28 +0000

    Adds actions for download and update

--- app/scripts/app-components/gel-scoring/score.js
+++ app/scripts/app-components/gel-scoring/score.js
@@ -7,10 +7,11 @@ define([
   "use strict";
 
   var templateScore = _.template(partialScoring);
-
+  var getRoot;
+  
   return function(context) {
     var view  = $("<div id='scoring-resource'></div>");
-    
+    getRoot = context.getS2Root;
     return {
       view:   view,
       events: {
@@ -28,6 +29,33 @@ define([
       yLabels: _.times(resource.number_of_rows, function(n) { return String.fromCharCode("A".charCodeAt(0)+n); })
     });
     view.append(scoringHtml);
+    
+    $("[data-action-s2=download]").click(_.partial(download, view, resource));
+    $("[data-action-s2=save]").click(_.partial(save, view, resource));
+  }
+  
+  function download(view, resource) {
+    getRoot().then(function(root) {
+      root.findGelImageByGel(resource.uuid).then(function(gelImage){
+        console.dir(gelImage);
+      });
+    });
+  }
+  
+  function save(view, resource) {
+    var data = {
+      uuid: resource.uuid,
+      scores: _.chain($("td[data-position-s2]", view)).map(function(td) {
+        var pos = $(td).attr("data-position-s2");
+        var value = $("select", td).val();
+        return [pos, value];
+      }).object().value()
+    };
+    getRoot().then(function(root) {
+      root.qualityActions.create.call(_.extend({
+        resourceType: "update_gel_image_score", 
+        root: root
+      }, root.qualityActions.update_gel_image_scores), data);
+    });
   }
 });
-  
