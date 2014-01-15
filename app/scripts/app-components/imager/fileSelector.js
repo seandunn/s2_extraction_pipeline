946459fac93ed1f520eebf010ef6df96917c068d
Eduardo Martin Rojo <emr@mib105656i.local>
2014-01-15 23:43:17 +0000

    Refactor/rewrite of imager code

--- app/scripts/app-components/imager/fileSelector.js
+++ app/scripts/app-components/imager/fileSelector.js
@@ -3,10 +3,25 @@ define(["text!app-components/imager/_fileSelector.html"
   "use strict";
   
   return function(context) {
-    var html = $(_.template(fileSelectorTemplate)(context));
+    // Instance
+    var instance = {
+      view: $(_.template(fileSelectorTemplate)(context)),
+      events: {
+        "onSelectedFile": _.identity
+      },
+      enable: function() {
+        this.view.attr("disabled", false);
+      },
+      disable: function() {
+        this.view.attr("disabled", true);
+      }      
+    };
+    // Private attributes
+    var html       = instance.view;
     var file       = html.find("input[type=file]");
     var filename   = html.find(".filename");
     
+    // Private event handling
     $(html[0]).on("click", function() {
       file.val('').click();
       $("input", html).click();
@@ -16,29 +31,21 @@ define(["text!app-components/imager/_fileSelector.html"
       handleFile(html, filename, event.originalEvent.target.files[0]);
     });
     
-    function handleFile(html, filename, file) {
+    // Private methods
+    var handleFile = _.bind(function(html, filename, file) {
       var data = _.clone(file);
       var reader       = new FileReader();
       reader.onload    = function(event) {
         $(".filename").text("File: " + file.name);
       };
-      reader.onloadend = function(event) {
+      reader.onloadend = _.bind(function(event) {
         if (event.target.readyState === FileReader.DONE) {
-          if (context.action) {
-            html.trigger(context.action, _.extend(data, { content: event.target.result}));
-          }
-          html.trigger("uploaded.request.imager.s2", _.extend(data, { content: event.target.result}));
-          html.trigger("done.s2");
+          this.events.onSelectedFile(_.extend(data, { content: event.target.result}));
         }
-      };
+      }, this);
       reader.readAsDataURL(file, 'UTF-8');
-    }
-        
-    return {
-      view: html,
-      events: {
-        "activate.s2": $.haltsEvent($.ignoresEvent(_.partial(_.bind(html.attr, html), "disabled", false)))
-      }
-    };
+    }, instance);
+
+    return instance;
   };
 });
