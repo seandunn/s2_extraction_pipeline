beed166dfa40ec700fd81e9594944cb36e747d90
Eduardo Martin Rojo <emr@mib105656i.local>
2014-01-15 23:43:17 +0000

    Refactor/rewrite of imager code

--- app/scripts/app-components/imager/imager.js
+++ app/scripts/app-components/imager/imager.js
@@ -1,33 +1,76 @@
 define([ "app-components/linear-process/switchable-linear-process", "app-components/labware/display", "labware/standard_mappers",
-    "app-components/imager/button", "app-components/imager/fileSelector",
-], function(linearProcess, labwareDisplay, representer, button, fileSelector) {
+    "app-components/imager/button", "app-components/imager/fileSelector", 
+    "app-components/imager/connect"
+], function(linearProcess, labwareDisplay, representer, Button, FileSelector, connect) {
   "use strict";
   
   return function(context) {
-    var LABWARE_DISPLAY="display.labware.s2";
-    var linear = linearProcess({ 
-      components : [
-        { constructor : _.partial(button, { text : "Begin Imager", action: "begin.imager.s2" }) },
-        { constructor : _.partial(button, { text : "End Imager", action: "completed.imager.s2" }) },
-        { constructor : _.partial(fileSelector, { text : "Select", action: "uploaded.file.imager.s2" }) },
-        { constructor : _.partial(button, { text : "Upload", action: "upload.request.imager.s2", notDisable: true }) }
-      ]});
-    
-    var labware = labwareDisplay({});
-    linear.view.on(labware.events);
-    linear.view.on("login.imager.s2", function() {
-      window.location.href=window.location.href;
-    });
-    
-    linear.view.trigger(LABWARE_DISPLAY, representer(context.labware));
-    linear.view.addClass("imager");
-    var html = labware.view;
-    html.append(linear.view);
-    var component = {
-      view: html,
-      events: linear.events
+    var instance = {
+      view: "",
+      events: {
+        "onBegin": function() {
+          beginBut.disable(); 
+          endBut.enable();
+          return arguments;
+        },
+        "onEnd": function() {
+          endBut.disable(); 
+          fileSelector.enable();
+          return arguments;          
+        },
+        "onSelectedFile": function() {
+          uploadBut.enable();
+          return arguments;          
+        },
+        "onUploadedFile": function() {
+          fileSelector.disable();
+          uploadBut.disable();
+          return arguments;          
+        },
+        "onDone": function() {
+          var ref = window.location.href;
+          ref = ref.replace(/#.*/, "");
+          window.location.href = ref;          
+        }
+      }
     };
     
-    return component;
+    // Private attributes
+    var LABWARE_DISPLAY="display.labware.s2";    
+    var beginBut, endBut, fileSelector, uploadBut, 
+        doneBut, labware;
+
+    // Private methods
+    var render = _.bind(function() {
+      beginBut = new Button({ text : "Begin Imager"});
+      endBut = new Button({ text : "End Imager"});
+      fileSelector = new FileSelector({ text : "Select"});
+      uploadBut = new Button({ text : "Upload"});
+      doneBut = new Button({ text : "Done"});
+      labware = labwareDisplay({});
+      
+      labware.view.append(_.pluck([beginBut, endBut, 
+                                   fileSelector, uploadBut, 
+                                   doneBut], "view"));
+      labware.view.on(labware.events);
+      labware.view.trigger(LABWARE_DISPLAY, representer(context.labware));
+      labware.view.addClass("imager");
+      
+      instance.view = labware.view;
+    }, instance);
+    
+    var attachHandlers = _.bind(function() {
+      // Attach handlers
+      connect(beginBut.events, 'onClicked', this.events, 'onBegin');
+      connect(endBut.events, 'onClicked', this.events, 'onEnd');
+      connect(fileSelector.events, 'onSelectedFile', this.events, 'onSelectedFile');
+      connect(uploadBut.events, 'onClicked', this.events, 'onUploadedFile');
+      connect(doneBut.events, 'onClicked', this.events, 'onDone');
+    }, instance);
+
+    render();
+    attachHandlers();
+    
+    return instance;
   };
 });
