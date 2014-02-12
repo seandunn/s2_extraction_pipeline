0686d883fd3fcb05ea01b96cf62d0e3f1b50011b
Sean Dunn <sd9@sanger.ac.uk>
2014-02-12 15:02:18 +0000

    Integrate graphing with lab-activities

--- app/scripts/app-components/lab-activities/lab-activities.js
+++ app/scripts/app-components/lab-activities/lab-activities.js
@@ -1,14 +1,18 @@
 define([
   "app-components/process-choice/process-choice",
   "app-components/re-racking/re-racking",
-  "app-components/gel-scoring/gel-scoring"], function(ProcessChoice, Reracking, GelScoring) {
+  "app-components/gel-scoring/gel-scoring",
+  "app-components/pipeline-graph/pipeline-graph"
+], function(ProcessChoice, Reracking, GelScoring, PipelineGraph) {
   "use strict";
 
   return function(context) {
     return new ProcessChoice(_.extend({
       components: [
         {label: "Re-racking", id: "re-racking", constructor: Reracking},
-        {label: "Gel Scoring", id: "gel-scoring", constructor: GelScoring}
+        {label: "Gel Scoring", id: "gel-scoring", constructor: GelScoring},
+        {label: "Pipeline Graph", id: "pipeline-graph", constructor: PipelineGraph}
+
       ]
     }, context));
   };
